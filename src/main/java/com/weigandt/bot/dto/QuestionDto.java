9dbceaf56f850bd7e4dafe0d871a8d7d516a580d
Konstantin Umanets <konstantin.umanets@weigandt-consulting.com>
2023-06-20 11:31:25 +0400

    Remove extra constructor

--- src/main/java/com/weigandt/bot/dto/QuestionDto.java
+++ src/main/java/com/weigandt/bot/dto/QuestionDto.java
@@ -3,13 +3,16 @@ package com.weigandt.bot.dto;
 import com.slack.api.model.event.AppMentionEvent;
 import com.slack.api.model.event.MessageChangedEvent;
 import com.slack.api.model.event.MessageEvent;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Optional;
 
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class QuestionDto {
     @Setter
     private String question;
@@ -17,13 +20,6 @@ public class QuestionDto {
     private final String rawInputText;
     private final String user;
     private final String answerPrefix;
-
-    private QuestionDto(String rawInputText, String user, String answerPrefix, String threadTs) {
-        this.rawInputText = rawInputText;
-        this.user = user;
-        this.answerPrefix = answerPrefix;
-        this.threadTs = threadTs;
-    }
 
     public QuestionDto(String rawInputText, String user, String threadTs, String ts, String answerPrefix) {
         this(rawInputText, user, answerPrefix, Optional.ofNullable(threadTs).filter(StringUtils::isNotBlank).orElse(ts));
