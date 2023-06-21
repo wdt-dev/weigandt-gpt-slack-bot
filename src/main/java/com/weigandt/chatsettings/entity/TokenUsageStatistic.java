6719b2da8c1f4655e78e2e9020e843a38492d7b6
Konstantin Umanets <konstantin.umanets@weigandt-consulting.com>
2023-06-21 16:43:52 +0400

    Migrate to MongoDB

--- src/main/java/com/weigandt/chatsettings/entity/TokenUsageStatistic.java
+++ src/main/java/com/weigandt/chatsettings/entity/TokenUsageStatistic.java
@@ -4,19 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tokenusagestatictics")
+@Document("tokenusagestatictics")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Profile("tokens-restriction")
 public class TokenUsageStatistic {
-    @EmbeddedId
+    @Id
     private DateAndUser id;
     private Integer tokenUsedQuestion;
     private Integer tokenUsedAnswer;
