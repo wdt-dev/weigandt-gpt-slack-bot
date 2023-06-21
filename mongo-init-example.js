68ce4ca8b7f88583a223ca14ee16ec21fc73e513
Konstantin Umanets <konstantin.umanets@weigandt-consulting.com>
2023-06-21 16:46:20 +0400

    Env configs and init MongoDb script examples

---
+++ mongo-init-example.js
@@ -0,0 +1,16 @@
+db.auth('root', 'example')
+
+db.createUser({
+    user: 'botuser',
+    pwd: 'botuserpassword',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'usersettings',
+        },
+        {
+            role: 'read',
+            db: 'admin',
+        },
+    ],
+});
